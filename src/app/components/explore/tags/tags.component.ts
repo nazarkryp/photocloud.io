a0eb9ccb2196804e227932302e68588b52f2b53b
Nazarii Krypiak <nazarkryp@gmail.com>
2017-08-07 17:07:35 +0300

    Explore tags improvements

--- src/app/components/explore/tags/tags.component.ts
+++ src/app/components/explore/tags/tags.component.ts
@@ -1,28 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { MdDialog } from '@angular/material';
 import { Subscription, Observable } from 'rxjs/Rx';
 
+import { PostDetailsComponent } from '../../shared/post-details/post-details.component';
 import { Collection, Post, CurrentUser } from '../../../common/models';
-import { PostService } from '../../../services';
+import { PostService, AccountService } from '../../../services';
+
+import { NgProgressService } from 'ngx-progressbar';
 
 @Component({
     selector: 'app-tags',
     templateUrl: './tags.component.html',
     styleUrls: ['./tags.component.css']
 })
-export class TagsComponent implements OnInit {
-    private subscription: Subscription;
+export class TagsComponent implements OnInit, OnDestroy {
+    private routeSubscription: Subscription;
     private page: Collection<Post> = new Collection<Post>();
+    private currentUser: CurrentUser;
 
     constructor(
+        public dialog: MdDialog,
         private route: ActivatedRoute,
-        private postService: PostService) {
+        private postService: PostService,
+        private accountService: AccountService,
+        private progressService: NgProgressService) {
+        this.currentUser = this.accountService.getCurrentUser();
     }
 
     private getPosts(tag: string) {
-        this.postService.getPostsByTag(tag, this.page.pagination)
-            // this.postService.getPosts(this.page.pagination)
+        this.progressService.start();
+        // this.postService.getPostsByTag(tag, this.page.pagination)
+        this.postService.getUserPosts('lanafeshchuk', this.page.pagination)
+            .finally(() => {
+                this.progressService.done();
+            })
             .subscribe(page => {
                 this.page.hasMoreItems = page.hasMoreItems;
                 this.page.pagination = page.pagination;
@@ -32,10 +44,54 @@ export class TagsComponent implements OnInit {
             });
     }
 
+    private like(post) {
+        if (post.userHasLiked) {
+            post.likesCount--;
+            post.userHasLiked = !post.userHasLiked;
+            this.postService.removePostLike(post.id)
+                .subscribe(() => {
+                    post.userHasLiked = false;
+                }, (error) => {
+                    if (post.userHasLiked) {
+                        post.likesCount--;
+                    } else {
+                        post.likesCount++;
+                    }
+                    post.userHasLiked = !post.userHasLiked;
+                    return error;
+                });
+        } else {
+            post.likesCount++;
+            post.userHasLiked = !post.userHasLiked;
+            this.postService.likePost(post.id)
+                .subscribe(() => {
+                    post.userHasLiked = true;
+                }, (error) => {
+                    if (post.userHasLiked) {
+                        post.likesCount--;
+                    } else {
+                        post.likesCount++;
+                    }
+                    post.userHasLiked = !post.userHasLiked;
+                    return error;
+                });
+        }
+    }
+
+    private openPostDialog(post: Post) {
+        const dialogRef = this.dialog.open(PostDetailsComponent, {
+            data: post
+        });
+    }
+
     public ngOnInit() {
-        this.subscription = this.route.params.subscribe(async params => {
+        this.routeSubscription = this.route.params.subscribe(async params => {
             const tag = params['tag'] as string;
             this.getPosts(tag);
         });
+    }
+
+    public ngOnDestroy(): void {
+        this.routeSubscription.unsubscribe();
     }
 }
