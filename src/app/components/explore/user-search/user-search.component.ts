a81a5946fcdd40a75ef28ff78756672dcbbb8175
Nazarii Krypiak <nazarkryp@gmail.com>
2017-09-06 09:29:59 +0300

    Users search improvements

--- src/app/components/explore/user-search/user-search.component.ts
+++ src/app/components/explore/user-search/user-search.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Rx';
 
 import { UserService } from '../../../services';
-import { Collection, User, RelationshipAction, RelationshipStatus } from '../../../common/models';
+import { UserProvider } from '../../../infrastructure/providers';
+import { Collection, User, CurrentUser, RelationshipAction, RelationshipStatus } from '../../../common/models';
 
 import { NgProgressService } from 'ngx-progressbar';
 
@@ -10,14 +12,23 @@ import { NgProgressService } from 'ngx-progressbar';
     templateUrl: './user-search.component.html',
     styleUrls: ['./user-search.component.css']
 })
-export class UserSearchComponent implements OnInit {
+export class UserSearchComponent implements OnInit, OnDestroy {
     private title = 'Explore People';
     private isLoading: boolean;
     private page: Collection<User> = new Collection<User>();
+    private modifying: { [id: number]: boolean } = {};
+    private currentUser: CurrentUser;
+    private currentUserSubscription: Subscription;
 
     constructor(
         private userService: UserService,
-        private progressService: NgProgressService) { }
+        private userProvider: UserProvider,
+        private progressService: NgProgressService) {
+        this.currentUserSubscription = this.userProvider.getCurrentUserAsObservable()
+            .subscribe((user) => {
+                this.currentUser = user;
+            });
+    }
 
     private getUsers() {
         this.isLoading = true;
@@ -28,6 +39,13 @@ export class UserSearchComponent implements OnInit {
                 this.progressService.done();
                 this.isLoading = false;
             })
+            .map((page: Collection<User>) => {
+                page.data.map((user: User) => {
+                    this.modifying[user.id] = false;
+                });
+
+                return page;
+            })
             .subscribe((page: Collection<User>) => {
                 this.page.hasMoreItems = page.hasMoreItems;
                 this.page.pagination = page.pagination;
@@ -37,7 +55,35 @@ export class UserSearchComponent implements OnInit {
             }, () => { });
     }
 
+    private modifyRelationship(user: User) {
+        this.modifying[user.id] = true;
+        const action = this.getRelationshipAction(user.incommingStatus);
+        this.userService.modifyRelationship(user.id, { action: action })
+            .finally(() => {
+                this.modifying[user.id] = false;
+            })
+            .subscribe((userResponse: User) => {
+                user.incommingStatus = userResponse.incommingStatus;
+            }, error => { });
+    }
+
+    private getRelationshipAction(incommingStatus: RelationshipStatus): number {
+        if (incommingStatus === RelationshipStatus.Following) {
+            return 1;
+        } else if (incommingStatus === RelationshipStatus.Requested) {
+            return 1;
+        } else if (incommingStatus === RelationshipStatus.Blocked) {
+            return 4;
+        }
+
+        return 0;
+    }
+
     public ngOnInit() {
         this.getUsers();
+    }
+
+    public ngOnDestroy() {
+        this.currentUserSubscription.unsubscribe();
     }
 }
